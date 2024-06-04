1de9895caa8ec0b93e09c455bad7f25e16798a63
Pedro Alves <pedro_172015@hotmail.com>
2024-06-04 11:24:26 -0300

    tela de login e cadastro

--- script.js
+++ script.js
@@ -46,6 +46,66 @@ function calcularIMC(){
 
     teclaCalcular.addEventListener('click',calcularIMC);
 
+    // SCRIPT DA ÁREA DE CADASTRO
+
+     // Array para armazenar os dados
+     let listaClientes = [];
+
+     // Função para adicionar um novo item à lista
+     function adicionarItem(name, email, peso, altura, imc) {
+       listaClientes.push({ name, email,peso, altura, imc });
+       atualizarTabela();
+     }
+ 
+     // Função para atualizar a tabela
+     function atualizarTabela() {
+       const tableBody = document.getElementById('tableBody');
+       tableBody.innerHTML = '';
+       listaClientes.forEach(item => {
+         const row = `<tr>
+                     <td>${item.name}</td>
+                     <td>${item.email}</td>
+                     <td>${item.peso}</td>
+                     <td>${item.altura}</td>
+                     <td>${item.imc.toFixed(2)}</td>
+                     <td>
+                       <button type="button" class="btn btn-danger btn-sm" onclick="excluirItem('${item.email}')">Excluir</button>
+                     </td>
+                   </tr>`;
+         tableBody.innerHTML += row;
+       });
+     }
+ 
+     // Função para calcular o Índice de Massa Corporal (IMC)
+     function calcularIMC(peso, altura) {
+       // Fórmula do IMC: IMC = peso / (altura * altura)
+       const imc = peso / (altura * altura);
+       return imc;
+     }
+ 
+     // Função para excluir um item da lista
+     function excluirItem(email) {
+       listaClientes = listaClientes.filter(item => item.email !== email);
+       atualizarTabela();
+     }
+ 
+     // Evento de envio do formulário
+     document.getElementById('crudForm').addEventListener('submit', function (event) {
+       event.preventDefault();
+       const name = document.getElementById('name').value;
+       const email = document.getElementById('email').value;
+       const peso = document.getElementById('peso').value;
+       const altura = document.getElementById('altura').value;
+       const imc = calcularIMC(peso, altura);
+       adicionarItem(name, email, peso, altura, imc);
+ 
+       // Limpar campos do formulário
+       document.getElementById('name').value = '';
+       document.getElementById('email').value = '';
+       document.getElementById('peso').value = '';
+       document.getElementById('altura').value = '';
+     });
+
 
 
 
